e4b370963696436fac4ef8cf89efc11258b8c768
Bryson <brysonmg@gmail.com>
2021-02-25 19:43:31 -0600

    Saving progress on broken code. Will fix when I get help

---
+++ Scripts/FishList.js
@@ -0,0 +1,15 @@
+import { getFish } from './FishData';
+
+export const FishList = () => {
+
+    // Get a reference to the `<article class="content">` element
+    const contentElement = document.querySelector(".fishTank")
+    const fishes = getFish()
+
+    // Add to the existing HTML in the content element
+    contentElement.innerHTML += `
+        <section class="fishList">
+            All the fish go here!
+        </section>
+    `
+}
